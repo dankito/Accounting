fc1079b61c1eaea74b80960c1ba02bfd14ecefe6
dankl <dankito@users.noreply.github.com>
2020-01-06 17:07:37 +0100

    Made FormattedInvoice open

--- AccountingCommon/src/main/kotlin/net/dankito/accounting/data/model/invoice/FormattedInvoice.kt
+++ AccountingCommon/src/main/kotlin/net/dankito/accounting/data/model/invoice/FormattedInvoice.kt
@@ -3,7 +3,7 @@ package net.dankito.accounting.data.model.invoice
 import net.dankito.accounting.data.model.NaturalOrLegalPerson
 
 
-class FormattedInvoice(val items: List<FormattedInvoiceItem>,
+open class FormattedInvoice(val items: List<FormattedInvoiceItem>,
                        val invoicingDate: String,
                        val invoiceNumber: String,
                        val netAmount: String,
